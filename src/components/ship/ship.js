508897a1c160a3d3c8e4ba8b05ef19002a4a90a5
uche-inyama <depoulet@yahoo.com>
2020-02-10 04:49:43 +0100

    implement ship factory

---
+++ src/components/ship/ship.js
@@ -0,0 +1,20 @@
+const Ship = (length, name) => {
+  let hitPositions = Array(length).fill(0);
+
+  const isSunk = () => hitPositions.every(currentValue => currentValue === 1);
+
+  const hit = index => {
+    hitPositions[index] = 1;
+    return hitPositions;
+  };
+
+  return {
+    length,
+    hitPositions,
+    isSunk,
+    hit,
+    name
+  };
+};
+
+module.exports = Ship;
