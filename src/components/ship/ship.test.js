2374c3d7331ba41538dcfe2f427c8242ee622465
uche-inyama <depoulet@yahoo.com>
2020-02-11 10:08:23 +0100

    implement jest test for ship component

---
+++ src/components/ship/ship.test.js
@@ -0,0 +1,9 @@
+const Ship = require('./ship');
+
+it('checks if ship, is sinking', () => {
+  expect(Ship(2).isSunk()).toBe(false);
+});
+
+it('checks if hitposition contains 1', () => {
+  expect(Ship(2).hit(0)).toContain(1);
+});
